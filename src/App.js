f10ffb07ad3836ae2897cda5c43af9e382984fb9
Daniel Tonon <dan503.npm@gmail.com>
2022-01-07 15:52:21 +1100

    Installing `@time-input-polyfill/react`

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg'
+import TimeInput from '@time-input-polyfill/react'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+	const [value, setValue] = useState()
 	return (
 		<div className="App">
 			<header className="App-header">
+				<TimeInput value={value} setValue={setValue} />
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
